87c2717ffa268ed50f671265555d9043bb01fa43
Micha Birklbauer <t0xic-m@users.noreply.github.com>
2020-12-15 17:41:30 +0100

    add scripts

---
+++ data/pgn_to_json.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import chess.pgn
+import re
+
+def get_data(pgn_file):
+    node = chess.pgn.read_game(pgn_file)
+    while node is not None:
+        data = node.headers
+
+        data["moves"] = []
+        data["timestamp"] = []
+
+        while node.variations:
+            next_node = node.variation(0)
+            data["moves"].append(
+                    re.sub("\{.*?\}", "", node.board().san(next_node.move)))
+            #data["timestamp"].append(re.findall("\{.*?\}", node.board().san(next_node.move))[0])
+            data["timestamp"].append(re.findall(".\:..\:..\..", node.board().san(next_node.move))[0])
+            node = next_node
+
+        node = chess.pgn.read_game(pgn_file)
+
+        out_dict = {}
+
+        for key in data.keys():
+            out_dict[key] = data.get(key)
+
+        # log(data.get('Event'))
+        yield out_dict
+
+
+def convert_file(file_path):
+    file_name = file_path.split(".")[0] + '.json'
+    out_list = []
+    try:
+        json_file = open(file_name, 'w')
+        pgn_file = open(file_path, encoding='utf-8')
+
+        for count_d, data in enumerate(get_data(pgn_file), start=0):
+            out_list.append(data)
+
+        json.dump(out_list, json_file)
+        json_file.close()
+    except Exception as e:
+        print(e)
+        print('ERROR file '+file_name+' not converted')
