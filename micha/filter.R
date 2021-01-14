76887e2aaf0f0fffa4f998a2930303ce582043ec
Micha Birklbauer <t0xic-m@users.noreply.github.com>
2021-01-14 16:58:27 +0100

    added heatmaps

--- micha/filter.R
+++ micha/filter.R
@@ -5,4 +5,13 @@ df <- df[which(df$player=="Alfons2357"),]
 df <- df[which(df$color=="White"),]
 df <- df[which(df$turn<=20),]
 
-write.csv2(df, file = "df.csv", row.names = F)
+write.csv2(df, file = "df_white.csv", row.names = F)
+
+df = read.csv2("chess_com_games_2020-12-15_2020-12-08.csv", sep = ",")
+df$X = NULL
+
+df <- df[which(df$player=="Alfons2357"),]
+df <- df[which(df$color=="Black"),]
+df <- df[which(df$turn<=20),]
+
+write.csv2(df, file = "df_black.csv", row.names = F)
