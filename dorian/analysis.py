a1315a778de6a5c36e6c0a08cd2bd436e1f9859f
dorian1000 <dodou@live.de>
2021-01-16 17:52:41 +0100

    new petrie net in analysis

--- dorian/analysis.py
+++ dorian/analysis.py
@@ -4,26 +4,34 @@
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
 from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
 from pm4py.evaluation.replay_fitness import evaluator as replay_fitness_evaluator
+from pm4py.visualization.petrinet import visualizer as pn_visualizer
 
 #%%
+# CONFIGURATION 
 log = xes_importer.apply("C54_log.xes")
 log_alfons_small = xes_importer.apply("enrich_df_alfons_small.xes")
 log_alfons_italian = xes_importer.apply("enrich_df_alfons_italian-game.xes")
 results = {}
 
-#%%
-filename = "result_im.json"
-net, initial_marking, final_marking = inductive_miner.apply(log)
+filename = None
+#filename = "result_am.json"
+#filename = "result_im.json"
+#filename = "result_hm.json"
+
+net = initial_marking = final_marking = None
+#net, initial_marking, final_marking = alpha_miner.apply(log)
+#net, initial_marking, final_marking = inductive_miner.apply(log)
+#net, initial_marking, final_marking = heuristics_miner.apply(log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.99})
+
+if (net and initial_marking and final_marking) is None: 
+    raise Exception("This is for your safety. Check configuration")
 
 #%%
-replayed_traces = token_replay.apply(log, net, initial_marking, final_marking)
-M = sum([trace['missing_tokens'] for trace in replayed_traces])
-C = sum([trace['consumed_tokens'] for trace in replayed_traces])
-R = sum([trace['remaining_tokens'] for trace in replayed_traces])
-P = sum([trace['produced_tokens'] for trace in replayed_traces])
-fitness_log_net_tbr = 1/2 * ( 1-(M/C) ) + 1/2 * ( 1-(R/P) )  
+gviz = pn_visualizer.apply(net, initial_marking, final_marking)
+pn_visualizer.view(gviz)
 
 #%%
 # fit log against model
@@ -53,7 +61,7 @@ results['2-log_alfons_small-alignment_based'] = fitness_eval_ALIGNMENT_BASED
 
 
 # %%
-# fit alfons small against model
+# fit alfons italian against model
 fitness_eval_TOKEN_BASED = replay_fitness_evaluator.apply(
     log_alfons_italian, net, 
     initial_marking, final_marking,
